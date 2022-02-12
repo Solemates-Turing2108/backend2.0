181b69e9ca12bbf07d55758c50585aecc7d98839
Matthew Kimball <mekimba6@gmail.com>
2022-02-12 16:52:39 -0700

    uncomments uri line for environment

--- app.py
+++ app.py
@@ -29,11 +29,11 @@ CORS(app)
 #make sure you have your own .env on your computer
 #comment out when you plan to deploy from heroku
 
-uri = os.getenv('URI')
+# uri = os.getenv('URI')
 
 
 #uncomment line below when you want to deploy to heroku
-# uri = os.environ.get("URI")
+uri = os.environ.get("URI")
 
 engine = create_engine(f'{uri}')
 
