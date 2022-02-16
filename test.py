c74440ac4986c72450939c382e1d9842a2fc3acc
Matthew Kimball <mekimba6@gmail.com>
2022-02-15 17:15:13 -0700

    adds test to add shoe

--- test.py
+++ test.py
@@ -15,3 +15,15 @@ def test_add_user():
     assert user.name == 'test'
     assert user.email == 'test@email.com'
     assert type(user) is UsersObject
+
+def test_add_shoe():
+    shoe = ShoesObject(side="left", style="sneaker", size=7, photo_url='test_url', description='this is a test', brand='test_brand', user_id=3)
+
+    assert shoe.side == 'left'
+    assert shoe.style == 'sneaker'
+    assert shoe.size == 7
+    assert shoe.photo_url == 'test_url'
+    assert shoe.description == 'this is a test'
+    assert shoe.brand == 'test_brand'
+    assert shoe.user_id == 3
+    assert type(shoe) is ShoesObject
